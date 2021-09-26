ef4f6140b230456b07c39488f637b3f9cb95db17
zhangjy <zhangjy@163.com>
2021-09-26 20:13:44 +0800

    third commit

--- src/main/java/com/example/demo/GitTest.java
+++ src/main/java/com/example/demo/GitTest.java
@@ -7,7 +7,8 @@ package com.example.demo;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git!");
-        System.out.println("hello git!");
+        System.out.println("hello git1!");
+        System.out.println("hello git2!");
+        System.out.println("hello git3!");
     }
 }
