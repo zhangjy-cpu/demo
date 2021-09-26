c2ba43027bfeebb56d97738e99c3244cb6fa144f
zhangjy <zhangjy@163.com>
2021-09-26 19:51:11 +0800

    first commit

---
+++ src/main/java/com/example/demo/GitTest.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+/**
+ * @author zhangjy
+ * @description: TODO
+ * @date 2021/9/26 19:47
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
---
+++ src/main/java/com/example/demo/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+/**
+ * @author zhangjy
+ * @description: TODO
+ * @date 2021/9/14 18:44
+ */
+public class HelloWorld {
+}
